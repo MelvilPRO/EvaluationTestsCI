c0b69ccec8b1d06fc3bf08cc1a4c927e6bc4d6f5
MelvilPRO <127743721+MelvilPRO@users.noreply.github.com>
2025-01-22 16:36:05 +0100

    Rajout de tests unitaires pour Reservation

--- PratiqueWithCode/EvaluationSampleCode.UnitTests/Reservation.unittest.cs
+++ PratiqueWithCode/EvaluationSampleCode.UnitTests/Reservation.unittest.cs
@@ -4,8 +4,40 @@
     public sealed class ReservationUnitTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void CanBeCancelledBy_CancelledByAdmin_ReturnTrue()
         {
+            User user = new User();
+            Reservation reservation = new Reservation(user);
+
+            User admin = new User();
+            admin.IsAdmin = true;
+
+            bool result = reservation.CanBeCancelledBy(admin);
+
+            Assert.AreEqual(result, true);
+        }
+
+        [TestMethod]
+        public void CanBeCancelledBy_CancelledByMadeBy_ReturnTrue()
+        {
+            User user = new User();
+            Reservation reservation = new Reservation(user);
+
+            bool result = reservation.CanBeCancelledBy(user);
+
+            Assert.AreEqual(result, true);
+        }
+
+        [TestMethod]
+        public void CanBeCancelledBy_CancelledByNotAdminNotMadeBy_ReturnFalse()
+        {
+            User userBy = new User();
+            Reservation reservation = new Reservation(userBy);
+            User userRandom = new User();
+
+            bool result = reservation.CanBeCancelledBy(userRandom);
+
+            Assert.AreEqual(result, false);
         }
     }
 }
